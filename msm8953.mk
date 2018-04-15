431b2b3b06b193214c8095566a3d0b6dfad532c6
Demon Singur <demonsingur@gmail.com>
2018-04-15 10:55:05 +0000

    msm8953-common: turn light hal into a hidl service
    Change-Id: I4fa56abd38c04d8fa267dbab67e54fe4a617bfe1

--- msm8953.mk
+++ msm8953.mk
@@ -237,9 +237,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8953
+    android.hardware.light@2.0-service.xiaomi_msm8953
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
