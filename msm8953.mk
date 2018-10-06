6f123a41589a0cbf4a46dfb1929843d560d9335c
haran <harangovindan@gmail.com>
2018-10-06 13:35:59 +0200

    msm8953-common: wifi: Drop deprecated wifi flag and packages
    * Not needed anymore as they are useless

--- msm8953.mk
+++ msm8953.mk
@@ -378,12 +378,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     libcld80211 \
-    libqsap_sdk \
-    libQWiFiSoftApCfg \
     libwpa_client \
     hostapd \
-    dhcpcd.conf \
-    wificond \
     wpa_supplicant \
     wpa_supplicant.conf
 
