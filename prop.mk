abf857c501bb6c28c3b06badca6a6658c142016f
yourdeathwish <harangovindan@gmail.com>
2018-11-16 06:56:38 +0200

    msm8953-common: Drop Wifi Display support completely
    * Wifi display is deprecated
    * Google as removed it completely from pie to ensure that HDCP HAL works
      properly
    * https://github.com/LineageOS/android_frameworks_av/commit/d0a98fa05f0f6719b93d000c4638230af06e0b99

--- prop.mk
+++ prop.mk
@@ -76,7 +76,6 @@ debug.gralloc.enable_fb_ubwc=1 \
 debug.mdpcomp.logs=0 \
 debug.sf.hw=0 \
 dev.pm.dyn_samplingrate=1 \
-persist.debug.wfd.enable=1 \
 persist.demo.hdmirotationlock=false \
 persist.hwc.enable_vds=1 \
 persist.hwc.mdpcomp.enable=true \
