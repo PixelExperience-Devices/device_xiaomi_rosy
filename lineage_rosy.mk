55794b74cc1693b2c1e0914f8e9c72628c0e018e
Bruno Martins <bgcngm@gmail.com>
2018-12-21 08:46:21 +0000

    rosy: Consider lineage_rosy product via AOSP way

--- lineage_rosy.mk
+++ lineage_rosy.mk
@@ -14,12 +14,23 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/xiaomi/rosy/full_rosy.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from rosy device
+$(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := rosy
 PRODUCT_NAME := lineage_rosy
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 5
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
