0f37b3ddb4b7ba8f8fe3963f8c2aa9795e1f83d3
Pavan Kumar M <rpavan@codeaurora.org>
2018-12-25 13:38:07 +0000

    msm8953-common: rootdir: Add vendor prefix to data/ims vendor binaries
     * Added vendor prefix to vendor bins
     * starting rtpd  on bootup without waiting for DATA_DAEMON prop and restarting when property is set
     * Starting imsrcsd on bootup without waiting for DATAD property which will load libimsrcsbaseimpl and register HAL services
    
    Change-Id: I9531b889871f82c64dc9b4b4c167d83dba8350a3

--- prop.mk
+++ prop.mk
@@ -175,7 +175,7 @@ rild.libargs=-d/dev/smd0 \
 rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=20 \
-service.qti.ims.enabled=1 \
+vendor.service.qti.ims.enabled=1 \
 telephony.lteOnCdmaDevice=1 \
 persist.vendor.ims.dropset_feature=0
 
