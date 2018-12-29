bf0e41f564a0d1733044bd7d5701554b6a5c9dca
aragon12 <sm95007@gmail.com>
2018-12-29 08:38:25 +0530

    rosy: add device specific init

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -85,3 +85,12 @@ LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.baseband.sh
 LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+# Device Script
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.rosy.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.rosy.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
