4850b42490c4d7f804da09734cba4f0ba75e4e29
Revanth <princechalapathi16@gmail.com>
2019-01-14 13:04:19 +0530

    rosy: Drop Telephony Jar's * Defined in Source

--- device.mk
+++ device.mk
@@ -338,9 +338,6 @@ PRODUCT_PACKAGES += \
     libxml2 \
     ims-ext-common
 
-PRODUCT_BOOT_JARS += \
-    qti-telephony-common
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
