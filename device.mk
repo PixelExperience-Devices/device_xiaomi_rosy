953a6ca0fc8def671bc5c0a58f1b65314a6a54d5
Rashed Abdel-Tawab <rashed@linux.com>
2019-01-24 10:45:09 +0000

    rosy: Add QTI privileged app permissions

--- device.mk
+++ device.mk
@@ -382,6 +382,7 @@ PRODUCT_PACKAGES += \
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
 # Wifi
