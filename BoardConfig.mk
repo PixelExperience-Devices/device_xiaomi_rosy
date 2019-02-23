678b3b1980e624f3620dcd7d48ce8f6286cc9a56
joshuous <joshuous@gmail.com>
2019-02-23 19:08:09 +0000

    Set DISABLE_EAP_PROXY to break wpa_supplicant dependency on qmi

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -232,5 +232,8 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+# Disable qmi EAP-SIM security
+DISABLE_EAP_PROXY := true
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/rosy/BoardConfigVendor.mk
