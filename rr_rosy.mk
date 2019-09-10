36492f2374779fedd56104f2d32503532ff7932c
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-09-11 01:16:45 +0530

    Update rr_rosy.mk

--- rr_rosy.mk
+++ rr_rosy.mk
@@ -17,7 +17,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
