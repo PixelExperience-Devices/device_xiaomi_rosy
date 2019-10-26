3e857fac1de693242a0a5336600c8eedd9bd692c
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2019-10-26 10:54:47 +0530

    rosy: drop biometrics

--- device.mk
+++ device.mk
@@ -173,10 +173,6 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_rosy
-
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
