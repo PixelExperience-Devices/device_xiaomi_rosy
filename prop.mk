21fd385015b42166fd916354f73504a215b2f68f
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2019-12-20 15:59:11 +0530

    rosy: some props moved to init.qcom.rc

--- prop.mk
+++ prop.mk
@@ -114,6 +114,19 @@ persist.gps.qc_nlp_in_use=1 \
 persist.loc.nlp_name=com.qualcomm.location \
 ro.gps.agps_provider=1
 
+#HWUI properties
+ro.hwui.texture_cache_size=72
+ro.hwui.layer_cache_size=48
+ro.hwui.r_buffer_cache_size=8
+ro.hwui.path_cache_size=32
+ro.hwui.gradient_cache_size=1
+ro.hwui.drop_shadow_cache_size=6
+ro.hwui.texture_cache_flushrate=0.4
+ro.hwui.text_small_cache_width=1024
+ro.hwui.text_small_cache_height=1024
+ro.hwui.text_large_cache_width=2048
+ro.hwui.text_large_cache_height=1024
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 av.debug.disable.pers.cache=1 \
@@ -189,7 +202,7 @@ ril.subscription.types=NV,RUIM \
 rild.libargs=-d/dev/smd0 \
 rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 ro.telephony.call_ring.multiple=false \
-ro.telephony.default_network=20 \
+ro.telephony.default_network=22,22 \
 vendor.service.qti.ims.enabled=1 \
 telephony.lteOnCdmaDevice=1 \
 persist.vendor.ims.dropset_feature=0 \
@@ -208,14 +221,6 @@ debug.sf.early_gl_app_phase_offset_ns=15000000
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.delta_time.enable=true
-
-# Tcp
-PRODUCT_PROPERTY_OVERRIDES += \
-net.tcp.2g_init_rwnd=10
-
-# Wifi
-PRODUCT_PROPERTY_OVERRIDES += \
-wifi.interface=wlan0
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
