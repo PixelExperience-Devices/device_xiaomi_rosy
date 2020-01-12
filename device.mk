fe485460f1f06e6be1e63d7c2c0e732116c76321
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-01-12 05:11:03 +0100

    rosy: drop init
    now wer are going with 2GB source dalvik-heap

--- device.mk
+++ device.mk
@@ -17,6 +17,7 @@
 $(call inherit-product, vendor/xiaomi/rosy/rosy-vendor.mk)
 $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
